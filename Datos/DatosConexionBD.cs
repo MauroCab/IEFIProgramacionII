3c7544da664e528a323885bd6cd748cb1a8ecb3f
Mauro Cabrera <maurocabricci17@gmail.com>
2024-10-28 08:02:49 -0300

    Primer Commit

---
+++ Datos/DatosConexionBD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DatosConexionBD
+    {
+        public DatosConexionBD()
+        {
+            conexion = new OleDbConnection(cadenaConexion);
+        }
+
+        public OleDbConnection conexion;
+        public string cadenaConexion = @"Provider=Microsoft.ACE.OLEDB.12.0;Data
+        Source=F:\EjemploBD.accdb;Persist Security Info=True";
+
+        public void Abrirconexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Broken || conexion.State ==
+                ConnectionState.Closed)
+                    conexion.Open();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al tratar de abrir la conexión", e);
+            }
+        }
+        public void Cerrarconexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Open)
+                    conexion.Close();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al tratar de cerrar la conexión", e);
+            }
+        }
+    }
+}
+}
---
+++ Negocios/NegItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Datos;
+using Entidades;
+
+namespace Negocios
+{
+    public class NegItems
+    {
+    }
+}

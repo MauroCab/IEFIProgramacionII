f81ecf58127d4249e93821e7b4c662801493fda6
Mauro Cabrera <maurocabricci17@gmail.com>
2024-10-28 07:41:42 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Conjunto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Conjunto
+    {
+        #region atributos
+
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region propiedades/encapsulamiento
+        #endregion
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        #region atributos
+        private string nombreUsuario;
+        private string email;
+        private string contrasena;
+        private List<Personaje> personajes;
+        #endregion
+
+        #region Constructor
+        public Usuario()
+        {
+
+        }
+        #endregion
+
+        #region propiedades/encapsulamiento
+        public string NombreUsuario 
+        { get { return nombreUsuario; }
+            set { nombreUsuario = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Contrasena
+        {
+            get { return contrasena; }
+            set { Contrasena = value; }
+        }
+
+        public List<Personaje> Personajes
+        {
+            get { return personajes; }
+            set { personajes = value; }
+        }
+        #endregion
+
+    }
+}
